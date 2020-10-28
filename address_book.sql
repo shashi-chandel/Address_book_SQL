a25218fa62eabb3cc970f276e8e2c8a9fab9d5cc
Your Name <you@example.com>
2020-10-28 22:50:25 +0530

    [shashi] Added : Address book upto usecase 8 is done

---
+++ address_book.sql
@@ -0,0 +1,51 @@
+#UC1
+CREATE DATABASE address_book_service; 		#Create database
+USE address_book_service; 			#To use address_book_service database	
+SELECT DATABASE(); 				#To see current database
+
+#UC2
+CREATE TABLE address_book   			#Create table address book
+(
+ first_name VARCHAR(50) NOT NULL,
+ last_name 	VARCHAR(50) NOT NULL,
+ address 	VARCHAR(100) NOT NULL,
+ city 		VARCHAR(50) NOT NULL,
+ state 		VARCHAR(50) NOT NULL,
+ zip 		VARCHAR(10) NOT NULL,
+ phone 		VARCHAR(15) NOT NULL,
+ email 		VARCHAR(100) NOT NULL
+);
+DESCRIBE address_book;
+
+#UC3
+INSERT INTO address_book VALUES  		#Data to insert
+	( 'Bill', 'Smith', 'Street 1', 'City 1', 'California', '123456', '9876543210', 'Bill@email.com' ),
+	( 'Terisa', 'Brown', 'Street 2', 'City 2','California', '123457', '9876543222', 'Terisa@email.com'),
+	( 'Charlie', 'Williams', 'Street 3', 'City 3', 'Florida', '223457', '8876543210', 'Charlie@email.com' );
+
+#UC4
+UPDATE address_book 				#Edit Contact using name
+SET zip = '023456' 
+WHERE first_name = 'Bill' && last_name = 'Smith';
+
+#UC5
+DELETE FROM address_book 			#Delete a contact using first name
+WHERE first_name = 'Terisa';
+
+#UC6
+SELECT * FROM address_book 			#Retrieve contact from a city
+WHERE city = 'City 1';
+SELECT * FROM address_book 			#Retrieve contact from a state
+WHERE state = 'Florida';
+
+#UC7 
+SELECT city,COUNT(city) FROM address_book GROUP BY city; 	#Retrieve count by city	
+SELECT state,COUNT(state) FROM address_book GROUP BY state; 	#Retrieve count by state
+
+#UC8	
+INSERT INTO address_book VALUES  		#Adding data to insert 
+	( 'Terisa', 'Brown', 'Street 2', 'City 2','California', '123457', '9876543222', 'Terisa@email.com'),
+	( 'Adams', 'Baker', 'Street 2', 'City 2','California', '123457', '9876543220', 'Adams@email.com'),
+	( 'Quinn', 'Smith', 'Street 5', 'City 2','California', '123459', '9876543229', 'Quinn@email.com');
+SELECT * FROM address_book WHERE city = 'City 2'
+ORDER BY first_name;
